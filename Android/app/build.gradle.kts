cbf78b042f6088184773a7e929d08ba99af6115c
Djanik <shamil@shamka.ru>
2024-02-09 12:09:34 +0300

    up version

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "mtm.cors_proxy"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
     }
 
