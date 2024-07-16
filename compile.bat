1cd4bd15ed0bd42b33742f1de8d6e887e9308a93
Shamil <shamil.kur@gmail.com>
2024-07-16 06:07:42 +0300

    fix compiler

--- compile.bat
+++ compile.bat
@@ -4,8 +4,8 @@ title CORS PROXY compiler
 cd "%~dp0"
 @rmdir /S /Q build
 @mkdir build
-echo javac -Xlint:deprecation -g:none -d build -s build\generated\ src\cors_proxy\*.java Android\app\src\main\java\cors_proxy\*.java src\android\annotation\*.java
-javac -Xlint:deprecation -g:none -d build -s build\generated\ src\cors_proxy\*.java Android\app\src\main\java\cors_proxy\*.java src\android\annotation\*.java
+echo javac --release 10 -Xlint:deprecation -g:none -d build -s build\generated\ src\cors_proxy\*.java Android\app\src\main\java\cors_proxy\*.java src\android\annotation\*.java
+javac --release 10 -Xlint:deprecation -g:none -d build -s build\generated\ src\cors_proxy\*.java Android\app\src\main\java\cors_proxy\*.java src\android\annotation\*.java
 xcopy res build\res\ /H /Y /C /R /S
 echo jar cfe cors_proxy.jar cors_proxy.Main -C build/ .
 jar cfe cors_proxy.jar cors_proxy.JarMain -C build/ .
